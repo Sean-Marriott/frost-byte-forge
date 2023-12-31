20e4be9b39b927dab27b8ae075c6e34467e9a5d3
Sean Marriott <seanwmarriott2110@gmail.com>
2023-12-31 18:44:13 +1300

    Reintroduced dashboard analytics

--- frost-byte-forge/app/page.tsx
+++ frost-byte-forge/app/page.tsx
@@ -1,5 +1,7 @@
 import Grid from "@mui/material/Unstable_Grid2";
 import Stack from "@mui/material/Stack";
+import DashboardAnalytics from "./ui/analytics/DashboardAnalytics";
+
 export default function Home() {
   return (
     <Grid container columnGap={2}>
@@ -7,7 +9,9 @@ export default function Home() {
         <Stack spacing={2}>
           <Stack spacing={2}>
             <h1>Dashboard</h1>
-            <div className="dashboard">{/* <DashboardAnalytics /> */}</div>
+            <div className="dashboard">
+              <DashboardAnalytics />
+            </div>
           </Stack>
           <Stack spacing={2}>
             <h2>Current Listings</h2>
